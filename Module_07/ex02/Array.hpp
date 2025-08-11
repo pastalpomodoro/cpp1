c611165b2ca2ae1650317a179e21eeb80d366e24
Thomas Gastelu <thomas@macbookpro.home>
2025-08-11 20:27:11 +0200

    salut

--- Module_07/ex02/Array.hpp
+++ Module_07/ex02/Array.hpp
@@ -12,6 +12,7 @@ class Array
     public:
         Array<T>(void) : _data(NULL), _size(0) {}
         Array<T>(const Array &copy){*this = copy;}
+        Array<T>(unsigned int n) : _data(new T[n]) {}
         Array<T>(T* data, unsigned int x) : _data(new T[x]), _size(x) {
             for (unsigned int i = 0; i < this->_size; i++){
                 this->_data[i] = data[i];
@@ -25,16 +26,32 @@ class Array
             if (this->_data != NULL)
                 delete this->_data;
             this->_data = new T[this->_size];
-            for (unsigned int i = this->_size; i < this->_size; i++){
+            for (unsigned int i = 0; i < this->_size; i++){
                 this->_data[i] = copy._data[i];
             }
             return (*this);
         }
+        T&      operator[](const unsigned int index){
+            if (index >= this->_size)
+                throw Array::OutOfBoundsException();
+            return (this->_data[index]);
+        }
+        class OutOfBoundsException : public std::exception
+        {
+            virtual const char *what() const throw(){
+                return ("Out Of Bounds Exception");
+            }
+        };
+        unsigned int size(void);
         void    setData(T *x);
         T*      getData(void);
         void    showData(void);
 };
 
+template <typename T>
+unsigned int Array<T>::size(void){
+    return (this->_size);
+}
 template <typename T>
 void    Array<T>::showData(void)
 {
--- Module_07/ex02/main.cpp
+++ Module_07/ex02/main.cpp
@@ -8,15 +8,26 @@ int main()
     a[2] = "hello";
     a[3] = "gutentagen";
 
+    // Array<std::string> b(a, 4);
+    // b.showData();
+    // std::cout << std::endl;
+    // std::string c[4];
+    // c[0] = "caca";
+    // c[1] = "pipi";
+    // c[2] = "";
+    // c[3] = "thomas";
+    // b.setData(c);
+    // b.showData();
+    
+    // std::cout << std::endl;
+    // Array<std::string> d(b);
+    // d.showData();
+    // d.setData(a);
+    // d.showData();
+    // std::cout << b.size() << std::endl;
+    // std::cout << d.size() << std::endl;
+    
     Array<std::string> b(a, 4);
-    b.showData();
-    std::cout << std::endl;
-    std::string c[4];
-    c[0] = "caca";
-    c[1] = "pipi";
-    c[2] = "";
-    c[3] = "thomas";
-    b.setData(c);
-    b.showData();
+    std::cout << b[4] << std::endl;
     delete[] a;
 }
