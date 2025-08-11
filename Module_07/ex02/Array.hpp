4760f830a578aeff6e5c82101fd8b6748cbb3834
Thomas Gastelu <thomas@macbookpro.home>
2025-08-11 17:32:33 +0200

    salut

--- Module_07/ex02/Array.hpp
+++ Module_07/ex02/Array.hpp
@@ -1,17 +1,59 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include<iostream>
+
 template <typename T>
 class Array
 {
     private:
-        T *_data;
+        T*           _data;
         unsigned int _size;
     public:
-        Array();
-        Array(const Array &copy);
-        ~Array();
-        Array &operator=(const Array &copy);
+        Array<T>(void) : _data(NULL), _size(0) {}
+        Array<T>(const Array &copy){*this = copy;}
+        Array<T>(T* data, unsigned int x) : _data(new T[x]), _size(x) {
+            for (unsigned int i = 0; i < this->_size; i++){
+                this->_data[i] = data[i];
+            }
+        }
+        ~Array<T>(void){delete [] _data;}
+        Array &operator=(const Array &copy){
+            if (this == &copy)
+                return (*this);
+            this->_size = copy._size;
+            if (this->_data != NULL)
+                delete this->_data;
+            this->_data = new T[this->_size];
+            for (unsigned int i = this->_size; i < this->_size; i++){
+                this->_data[i] = copy._data[i];
+            }
+            return (*this);
+        }
+        void    setData(T *x);
+        T*      getData(void);
+        void    showData(void);
 };
 
-#endif
+template <typename T>
+void    Array<T>::showData(void)
+{
+    for (unsigned int i = 0; i < this->_size; i++){
+        std::cout << this->_data[i] << std::endl;
+    }
+}
+
+template <typename T>
+void    Array<T>::setData(T *x)
+{
+    for (unsigned int i = 0; i < this->_size; i++){
+        this->_data[i] = x[i];
+    }
+}
+
+template <typename T>
+T*  Array<T>::getData(void)
+{
+    return (this->_data);
+}
+#endif  
--- Module_07/ex02/main.cpp
+++ Module_07/ex02/main.cpp
@@ -1,3 +1,22 @@
 #include "Array.hpp"
 
-int main(){;}
+int main()
+{
+    std::string *a = new std::string[4];
+    a[0] = "salut";
+    a[1] = "ciao";
+    a[2] = "hello";
+    a[3] = "gutentagen";
+
+    Array<std::string> b(a, 4);
+    b.showData();
+    std::cout << std::endl;
+    std::string c[4];
+    c[0] = "caca";
+    c[1] = "pipi";
+    c[2] = "";
+    c[3] = "thomas";
+    b.setData(c);
+    b.showData();
+    delete[] a;
+}
