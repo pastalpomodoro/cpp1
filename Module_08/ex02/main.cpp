e137efbc0f46f53cb9444994ecfc7d7068efa91c
Thomas Gastelu <thomas@MacBook-Pro-di-Arnaud-2.local>
2025-08-24 16:53:46 +0200

    cleaning server class sintax

--- Module_08/ex02/main.cpp
+++ Module_08/ex02/main.cpp
@@ -1,5 +1,5 @@
 #include"MutantStack.hpp"
-
+#include<list>
 int main(void){
     MutantStack<int> a;
     a.push(12);
@@ -10,4 +10,50 @@ int main(void){
         std::cout << *b << std::endl;
         b++;
     }
+    std::cout << "------------------------------------------\n";
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+    std::cout << *it << std::endl;
+    ++it;
+    }
+    std::stack<int> s(mstack);
+
+    std::cout << std::endl << std::endl;
+
+    std::list<int> mstack1;
+    mstack1.push_back(5);
+    mstack1.push_back(17);
+    std::cout << mstack1.back() << std::endl;
+    mstack1.pop_back();
+    std::cout << mstack1.size() << std::endl;
+    mstack1.push_back(3);
+    mstack1.push_back(5);
+    mstack1.push_back(737);
+    //[...]
+    mstack1.push_back(0);
+    std::list<int>::iterator it1 = mstack1.begin();
+    std::list<int>::iterator ite1 = mstack1.end();
+    ++it1;
+    --it1;
+    while (it1 != ite1)
+    {
+    std::cout << *it1 << std::endl;
+    ++it1;
+    }
+    return 0;
 }
